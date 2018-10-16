b0082a59f2ae69cc034268ac7345a1d8cd8026a8
Armand NIAMPA <armand@capsens.eu>
2018-10-16 07:06:35 +0200

    Boundaries validations

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -21,6 +21,36 @@ class Opening < ActiveRecord::Base
     validates :end_at
   end
 
+  # Timeliness validations
   validates_datetime :start_at, on: :create, on_or_after: :now, ignore_usec: true
   validates_datetime :end_at, after: :start_at
+
+  # Custom validations
+  validate :same_date
+  validate :overlapping
+
+  # Scopes
+  scope :overlapping, ->(start_at, end_at) do
+    args = { start_at: start_at, end_at: end_at }
+    where('start_at < :end_at AND end_at > :start_at', **args)
+  end
+
+  private
+
+  # Custom validation for model, to check overlapping time range
+  def overlapping
+    return unless calendar
+    overlapings = calendar.openings.overlaping(start_at, end_at)
+    return unless overlapings.any?
+    errors.add(:base, :overlapping)
+  end
+
+  # Custom validation for model, for a day boundaries
+  def same_date
+    return unless start_at? && end_at?
+    return unless start_at.to_date != end_at.to_date
+
+    errors.add(:start_at, :same_date)
+    errors.add(:end_at, :same_date)
+  end
 end
